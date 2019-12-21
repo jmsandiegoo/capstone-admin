cb93d530ceb7c67191f0a8c5ac5e29e893817beb
John Michael San Diego <jmsandiego0710@gmail.com>
2019-12-21 16:27:51 +0800

    implemented login functionality

---
+++ helpers/helper.php
@@ -0,0 +1,50 @@
+<?php
+class Helper
+{
+    private $defaultPath;
+
+    public function __construct()
+    {
+        $this->defaultPath = ($_SERVER['SERVER_NAME'] == 'localhost') ? '/capstone-admin/' : '/' ;
+    }
+
+    public function subviewPath($file) { // return a path to include subviews
+        $file =  __DIR__ . '/../views/sub-views/'. $file;
+        return $file;
+    }
+
+    function cssPath($file) { // return a css file path to for href attributes
+        $hrefPath = $this->defaultPath .'assets/css/'. $file;
+        return $hrefPath;
+    }
+
+    function jsPath($file) { // return a js file path to for src attributes
+        $hrefPath = $this->defaultPath . 'assets/js/'. $file;
+        return $hrefPath;
+    }
+
+    function pageUrl($file) { // return a page path for href navigation
+        $httpProtocol = !isset($_SERVER['HTTPS']) || $_SERVER['HTTPS'] != 'on' ? 'http' : 'https';
+        $url = $httpProtocol . '://' . $_SERVER['HTTP_HOST'] . $this->defaultPath;
+        if ($file !== 'index.php') {
+            $url .= 'views/' . $file;
+        }
+        return $url;
+    }
+
+    function flatten_array($array, $prefix = '' ) {
+        $result = array();
+        foreach ($array as $key => $value) {
+            
+        }
+    }
+    function subPageUrl($file) { // return a page path for href navigation
+        $httpProtocol = !isset($_SERVER['HTTPS']) || $_SERVER['HTTPS'] != 'on' ? 'http' : 'https';
+        $url = $httpProtocol . '://' . $_SERVER['HTTP_HOST'] . $this->defaultPath;
+        if ($file !== 'index.php') {
+            $url .= 'views/sub-views/' . $file;
+        }
+        return $url;
+    }
+}
+?>
---
+++ process/loginFunctions.php
@@ -0,0 +1,64 @@
+<?php 
+    include_once __DIR__. '/../helpers/helper.php';
+    include_once __DIR__. '/../helpers/mysql.php';
+    session_start();
+    $helper = new Helper();
+
+    if (isset($_POST["authenticate"])) {
+        $email = $_POST["email"];
+        $password = $_POST["password"];
+        // check if email field
+        if (empty($email) || empty($password)) {
+            $empty_error = "Please fill both <b> email and password </b> fields.";
+            $_SESSION["login_error"] = $empty_error;
+            $_SESSION["email"] = $email;
+            $_SESSION["password"] = $password;
+            $pageUrl = $helper->pageUrl('index.php');
+            header("Location: $pageUrl");
+            exit;
+        } else {
+            // check if account is valid
+            $db = new Mysql_Driver();
+
+            $db->connect();
+
+            $qry = "SELECT * FROM Account WHERE email = '$email'";
+
+            $result = $db->query($qry);
+
+            if ($db->num_rows($result) <= 0) {
+                echo "No results";
+                $login_error = "Sorry, it seems that the <b> email and/or password is incorrect </b>. Please try again.";
+            } else { 
+                echo "Got results";
+                $row = $db->fetch_array($result);
+                $userId = $row["id"]; 
+                $correctPassword = $row["password"];
+                echo "Password: $password". gettype($password) . "Correct: $correctPassword" . gettype($correctPassword);
+                echo $password === $correctPassword;
+                if ($password === $correctPassword) {
+                    echo "doesnt go here wtf?";
+                    session_regenerate_id();
+                    $_SESSION['loggedin'] = TRUE;
+                    $_SESSION['email'] = $email;
+                    $_SESSION['user_id'] = $userId;
+                    $pageUrl = $helper->pageUrl('home.php');
+                    header("Location: $pageUrl");
+                    exit;
+                }
+
+                $login_error = "Sorry, it seems that the <b> email and/or password is incorrect </b>. Please try again.";
+            }
+            $_SESSION["login_error"] = $login_error;
+            $_SESSION["email"] = $email;
+            $_SESSION["password"] = $password;
+            $pageUrl = $helper->pageUrl('index.php');
+            // header("Location: $pageUrl");
+            exit;
+        }
+
+    } else if (isset($_POST["forgotPassword"])) {
+
+    }
+
+?>
