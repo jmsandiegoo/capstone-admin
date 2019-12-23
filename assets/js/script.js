958a52ffb574967b701a24892263439c11b214b3
John Michael San Diego <jmsandiego0710@gmail.com>
2019-12-23 16:25:15 +0800

    Added basic layout for appointment table

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+/* ---------------------------------------------------
+    SIDE BAR JS
+----------------------------------------------------- */
+
+var sidebarBtn = document.querySelector('#sidebarCollapse');
+var sidebar = document.querySelector('#sidebar');
+
+sidebarBtn.addEventListener('click', () => {
+    sidebar.classList.toggle('active');
+})
--- views/appointment.php
+++ views/appointment.php
@@ -10,18 +10,50 @@
         exit;
     }
 
-
+    // $appointmentType = $GET["appointmentType"];
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
     <?php include $helper->subviewPath('header.php') ?>
-
     <main class="main-content-wrapper">
         <?php include $helper->subviewPath('sidebar.php')?>
         <!-- main content -->
         <section class="content">
             <?php include $helper->subviewPath('navbar.php')?>
+            <!-- Table View -->
+            <div class="table-wrapper">
+                <table class="table">
+                    <thead class="thead-dark">
+                        <tr>
+                        <th scope="col">Queue No.</th>
+                        <th scope="col">Name</th>
+                        <th scope="col">Waiting Time</th>
+                        <th scope="col">Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                        <th scope="row">1</th>
+                        <td>Mark</td>
+                        <td>Otto</td>
+                        <td>Skip or Call</td>
+                        </tr>
+                        <tr>
+                        <th scope="row">2</th>
+                        <td>Jacob</td>
+                        <td>Thornton</td>
+                        <td>Skip or Call</td>
+                        </tr>
+                        <tr>
+                        <th scope="row">3</th>
+                        <td>Larry</td>
+                        <td>the Bird</td>
+                        <td>Skip or Call</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         </section>
     </main>
     <?php include $helper->subviewPath('footer.php') ?>
