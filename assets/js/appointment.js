c576f804c17cf47cefc9ce0b162e0744e9080ec0
John Michael San Diego <jmsandiego0710@gmail.com>
2020-01-07 00:24:29 +0800

    Buttons now works but still need more work

--- assets/js/appointment.js
+++ assets/js/appointment.js
@@ -7,14 +7,14 @@ var course_id = getUrlParameter('course_id');
 
 // Fetch the pending List
 document.addEventListener("DOMContentLoaded", () => {
-    populatePendingTable(course_id);
-    setInterval(function(){ populatePendingTable(course_id); } , 5000);
+    getAjaxAppointments('Pending', course_id);
+    setInterval(function(){ getAjaxAppointments('Pending', course_id); } , 5000);
 });
 
 // Fetch Now Serving List
 document.addEventListener("DOMContentLoaded", () => {
-    populateNowServingTable();
-    setInterval(populateNowServingTable, 5000);
+    getAjaxAppointments('NowServing');
+    setInterval(function(){ getAjaxAppointments('NowServing', course_id); }, 5000);
 });
 
 // Functions
@@ -24,16 +24,6 @@ function getUrlParameter(name) {
     var results = regex.exec(location.search);
     return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
 };
-
-function populatePendingTable(course_id) {
-    // TO-DO
-    var response = getAjaxAppointments('Pending', course_id);
-}
-
-function populateNowServingTable() {
-    // TO-DO
-    var response = getAjaxAppointments('NowServing');
-}
 
 function getAjaxAppointments(type, course_id = null) {
     var xhr = new XMLHttpRequest();
@@ -48,11 +38,73 @@ function getAjaxAppointments(type, course_id = null) {
         console.log('type', type);
         console.log('response', response);
         if (response.status === 200) {
-            return response.data;
+            if (type == "Pending") {
+                populatePendingTable(response.data);
+            } else {
+                populateNowServingTable(response.data);
+            }
         } else { // if response is 400 or 404
             // redirect to server error TO-DO*
             return null;
         }
     }
     xhr.send();
+}
+
+function populatePendingTable(response) {
+    // TO-DO
+    var tableBody = document.querySelector('.pending-table tbody');
+    tableBody.innerHTML = "";
+
+    for (var i = 0; i < response.length; i++) {
+        var appointment = response[i];
+        var tr = document.createElement('tr');
+        tr.innerHTML = `<th scope="row"> ${appointment.appointment_id} </th>` + 
+                        `<td>${appointment.appointment_name}</td>` +
+                        `<td>${appointment.waiting_time}</td>` +
+                        `<td><form action="../process/appointmentFunctions.php" method="POST">` +
+                        `<input type="hidden" name="appointment_id" value="${appointment.appointment_id}" />` +
+                        `<button type="submit" class="btn btn-dark" name="call-submit">Call</button>` +
+                        `</form>` +
+                        `<form action="../process/appointmentFunctions.php" method="POST">` +
+                        `<input type="hidden" name="appointment_id" value="${appointment.appointment_id}" />` +
+                        `<button type="submit" class="btn btn-light" name="end-submit">Skip</button>` +
+                        `</form></td>`;
+        tableBody.appendChild(tr);
+    }
+
+    if (response.length == 0) {
+        var tr = document.createElement('tr');
+        tr.innerHTML = `<td colspan="100%" class="text-center">There are no pending appointments currently.</td>`;
+        tableBody.appendChild(tr);
+    }
+}
+
+function populateNowServingTable(response) {
+    // TO-DO
+    var tableBody = document.querySelector('.now-serving-table tbody');
+    tableBody.innerHTML = "";
+
+    for (var i = 0; i < response.length; i++) {
+        var appointment = response[i];
+        var tr = document.createElement('tr');
+        tr.innerHTML = `<th scope="row"> ${appointment.appointment_id} </th>` + 
+                        `<td>${appointment.appointment_name}</td>` +
+                        `<td>${appointment.last_called} </br> (Called: ${appointment.appointment_calls} times)</td>` + 
+                        `<td><form action="../process/appointmentFunctions.php" method="POST">` +
+                        `<input type="hidden" name="appointment_id" value="${appointment.appointment_id}" />` +
+                        `<button type="submit" class="btn btn-dark" name="end-submit">End</button>` +
+                        `</form>` +
+                        `<form action="../process/appointmentFunctions.php" method="POST">` +
+                        `<input type="hidden" name="appointment_id" value="${appointment.appointment_id}" />` +
+                        `<button type="submit" class="btn btn-light" name="recall-submit">Re-Call</button>` +
+                        `</form></td>`;
+        tableBody.appendChild(tr);
+    }
+
+    if (response.length == 0) {
+        var tr = document.createElement('tr');
+        tr.innerHTML = `<td colspan="100%" class="text-center">There are no appointments being served currently.</td>`;
+        tableBody.appendChild(tr);
+    }
 }
--- views/appointment.php
+++ views/appointment.php
@@ -12,30 +12,25 @@
     }
 
     $course_id = '';
+
     if (isset($_GET["course_id"])) {
-        $course_id = $_GET["course_id"];
+        $course_id = (int)$_GET["course_id"];
     }
-
-    $db->connect();
-
-    $appointmentWaitingResult = null;
-    $appointmentServingResult = null;
 
     $appointmentName = "General";
 
     if ($course_id) {
-
+        $db->connect();
         // Get the appointment name
-        $qry = "SELECT * FROM Course WHERE course_id = ?";
+        $qry = "SELECT * FROM Course WHERE id = ?";
 
         $courseResult = $db->query($qry, $course_id);
 
         $row = $db->fetch_array($courseResult);
-
         $appointmentName = $row["course_name"];
-    }
 
-    $db->close();
+        $db->close();
+    }
 
 ?>
 
@@ -52,7 +47,7 @@
                 <h2><?php echo $appointmentName ?></h2>
                 <hr>
                 <h3>Waiting</h3>
-                <table class="table table-bordered">
+                <table class="table table-bordered pending-table">
                     <thead class="thead-dark">
                         <tr>
                         <th scope="col">Queue No.</th>
@@ -62,38 +57,13 @@
                         </tr>
                     </thead>
                     <tbody>
-                        <!-- <tr>
-                        <th scope="row"><?php echo $row['appointment_id'] ?></th>
-                        <td><?php echo $row['appointment_name'] ?></td>
-                        <td>
-                            <?php echo $helper->secondsToTime($row['waiting_time']) ?>
-                        </td>
-                        <td class="action-cell">
-                            <form action="<?php echo $helper->processUrl('appointmentFunctions.php') ?>" method="POST">
-                                <input type="hidden" name="appointment_id" value="<?php echo $row['appointment_id'] ?>" />
-                                <?php if($course_id): ?>
-                                    <input type="hidden" name="course_id" value="<?php echo $course_id ?>" />
-                                <?php endif; ?>
-                                <button type="submit" class="btn btn-dark" name="call-submit">Call</button>
-                            </form>
-                            <form action="<?php echo $helper->processUrl('appointmentFunctions.php') ?>" method="POST">
-                                <input type="hidden" name="appointment_id" value="<?php echo $row['appointment_id'] ?>" />
-                                <?php if($course_id): ?>
-                                    <input type="hidden" name="course_id" value="<?php echo $course_id ?>" />
-                                <?php endif; ?>
-                                <button type="submit" class="btn btn-light" name="end-submit">Skip</button>
-                            </form>
-                        </td>
-                        </tr> -->
-                        <tr>
-                        <td colspan="100%" class="text-center">There are no pending appointments currently.</td>
-                        </tr>
+                        <!-- Populated by js -->
                     </tbody>
                     
                 </table>
                 
                 <h3>Now Serving</h3>
-                <table class="table table-bordered">
+                <table class="table table-bordered now-serving-table">
                     <thead class="thead-dark">
                         <tr>
                         <th scope="col">Queue No.</th>
@@ -103,32 +73,7 @@
                         </tr>
                     </thead>
                     <tbody>
-                        <!-- <tr>
-                        <th scope="row"><?php echo $row['appointment_id'] ?></th>
-                        <td><?php echo $row['appointment_name'] ?></td>
-                        <td>
-                            <?php echo $helper->secondsToTime($row['last_called']) ?>
-                        </td>
-                        <td class="action-cell">
-                            <form action="<?php echo $helper->processUrl('appointmentFunctions.php') ?>" method="POST">
-                                <input type="hidden" name="appointment_id" value="<?php echo $row['appointment_id'] ?>" />
-                                <?php if($course_id): ?>
-                                    <input type="hidden" name="course_id" value="<?php echo $course_id ?>" />
-                                <?php endif; ?>
-                                <button type="submit" class="btn btn-dark" name="end-submit">End</button>
-                            </form>
-                            <form action="<?php echo $helper->processUrl('appointmentFunctions.php') ?>" method="POST">
-                                <input type="hidden" name="appointment_id" value="<?php echo $row['appointment_id'] ?>" />
-                                <?php if($course_id): ?>
-                                    <input type="hidden" name="course_id" value="<?php echo $course_id ?>" />
-                                <?php endif; ?>
-                                <button type="submit" class="btn btn-light" name="recall-submit">Re-Call</button>
-                            </form>
-                        </td>
-                        </tr> -->
-                        <tr>
-                        <td colspan="100%" class="text-center">There are no appointments being served currently.</td>
-                        </tr>
+                        <!-- Populated by js -->
                     </tbody>
                     
                 </table>
